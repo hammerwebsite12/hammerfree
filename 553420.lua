d69cddd0e1df5345110d8da671fc3118e7dcc252
hammerwebsite12 <thepoorergamer2424@gmail.com>
2026-04-02 12:17:46 +0800

    Add files via upload

---
+++ 553420.lua
@@ -0,0 +1,23 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 553420 Manifest
+-- Name: TUNIC
+-- Generated: 2025-06-07 21:52:54
+-- Total Depots: 4
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(553420) -- TUNIC
+
+-- MAIN APP DEPOTS
+addappid(553422, 1, "07afa2104cb580674d8b8cb09b48c338768bf256a3555e10d3a5876a1a44aa06") -- Game Content (Linux Binaries)
+setManifestid(553422, "3001314299650182457", 0)
+addappid(553423, 1, "484b1bd155071096807adc360d658dc1041d5228e01ab0fe658f03e6738497e7") -- Game Content (Mac Content)
+setManifestid(553423, "2843757291572825323", 0)
+
+-- DLCS WITH DEDICATED DEPOTS
+-- TUNIC (Original Game Soundtrack) (AppID: 1928140)
+addappid(1928140)
+addappid(1928141, 1, "710d73c4141c73683893786213a450152dbeeed4d7158a106b522bf76baf7d41") -- TUNIC (Original Game Soundtrack) - Main Content
+setManifestid(1928141, "4702785820938042584", 0)
+addappid(1928142, 1, "2850ed92913d55211b7fdbe1b01c64ec8cd645ce0cec09508c854de7cf75b511") -- TUNIC (Original Game Soundtrack) - Main Content
+setManifestid(1928142, "8419190336620755006", 0)
