a95a9dc71e170b5f15773429bfa0b35365ce2ad6
hammerwebsite12 <thepoorergamer2424@gmail.com>
2026-04-02 12:16:15 +0800

    Add files via upload

---
+++ 413150.lua
@@ -0,0 +1,22 @@
+-- 413150's Lua and Manifest Created by Morrenus
+-- Stardew Valley
+-- Created: September 30, 2025 at 19:38:10 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(413150, 1, "2352cc75feedbdb8e9a6d3dd1f6a71af9d38068a6369af6ca83e4301abee4100") -- Stardew Valley
+-- MAIN APP DEPOTS
+addappid(413151, 1, "ff71699a17787b798d901cb27398556eb69a498b690b4392b2ffedcacc1019ff") -- Stardew Valley Windows
+setManifestid(413151, "4278718763097142923", 691846347)
+addappid(413152, 1, "c65e7919242bdcb29efd9551af381e9b6f5d828ac7382cec83f1d8459aaf3fdf") -- Stardew Valley Mac
+setManifestid(413152, "2618652301291810021", 711467377)
+addappid(413153, 1, "d1e04015717ef68e43064bd29a027c3309b47bd156d0494c7d4dff56a971e3c7") -- Stardew Valley Linux
+setManifestid(413153, "6005910083361727734", 708901690)
+-- SHARED DEPOTS (from other apps)
+addappid(229002, 1, "f95e8545cfe871c330a483b25520ba7ed3750d21b9a51791ff0f0ed0dae33738") -- .NET 4.0 Redist (Shared from App 228980)
+setManifestid(229002, "7260605429366465749", 50450161)
+addappid(229012, 1, "421e499f39a617c1a3b6baf11fbc65ffebd541d2c39c38d3bf6ffbd87919ce12") -- XNA 4.0 Redist (Shared from App 228980)
+setManifestid(229012, "4353723233161159493", 7061608)
